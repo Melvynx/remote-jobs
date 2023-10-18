fceffdf738e5ded1667434d0a4c230accee7e3aa
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2023-10-18 10:40:52 +0800

    add weworkremotly

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,15 +1,18 @@
 import { Prisma } from '@prisma/client';
 import pw, { Browser } from 'playwright';
 
-import { prisma } from '@/lib/prisma';
 import { NextResponse } from 'next/server';
+import { prisma } from '@/lib/prisma';
 
 const SBR_CDP = `wss://${process.env.BRIGHDATA_AUTH}@brd.superproxy.io:9222`;
 
 export const GET = async (req: Request, res: Response) => {
   const browser = await pw.chromium.connectOverCDP(SBR_CDP);
 
-  const jobs = await getRemoteOkJobs(browser);
+  const remoteOKJobs = await getRemoteOkJobs(browser);
+  const workRemotelyJobs = await getWorkRemotelyJobs(browser);
+
+  const jobs = [...remoteOKJobs, ...workRemotelyJobs];
 
   await prisma.jobs.createMany({
     data: jobs,
@@ -18,6 +21,71 @@ export const GET = async (req: Request, res: Response) => {
   return NextResponse.json({
     jobs,
   });
+};
+
+const getWorkRemotelyJobs = async (instance: Browser) => {
+  const page = await instance.newPage();
+  await page.goto(
+    'https://weworkremotely.com/categories/remote-full-stack-programming-jobs#job-listings'
+  );
+
+  const jobs = await page.$$eval('article li', (rows) => {
+    return rows.map((row) => {
+      if (row.classList.contains('ad')) return;
+
+      // ligne
+      const obj = {
+        title: '',
+        company: '',
+        date: new Date(),
+        logo: '',
+        salary: '',
+        url: '',
+      } satisfies Prisma.JobsCreateManyInput;
+
+      const title = row.querySelector('.title');
+
+      if (title) {
+        obj.title = title.textContent?.trim() ?? '';
+      }
+
+      const company = row.querySelector('.company');
+
+      if (company) {
+        obj.company = company.textContent?.trim() ?? '';
+      }
+
+      const divLogo = row.querySelector('.flag-logo') as HTMLDivElement;
+
+      if (divLogo) {
+        const backgroundImage = divLogo.style.backgroundImage;
+        const img = backgroundImage
+          ?.replace('url(', '')
+          .replace(')', '')
+          .replaceAll('"', '');
+        obj.logo = img;
+      }
+
+      // get the direct a tag child
+      const aElement = row.querySelectorAll('a')[1];
+      if (aElement) {
+        obj.url =
+          'https://weworkremotely.com/' + aElement.getAttribute('href') ?? '';
+      }
+
+      return obj;
+    });
+  });
+
+  const jobsFiltered = jobs.filter((job) => {
+    if (!job) return false;
+    if (!job?.title) return false;
+    if (!job?.url) return false;
+    if (!job?.company) return false;
+    return true;
+  }) as Prisma.JobsCreateManyInput[];
+
+  return jobsFiltered;
 };
 
 const getRemoteOkJobs = async (instance: Browser) => {
